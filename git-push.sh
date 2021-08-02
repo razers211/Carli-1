62fb31f112a80ec911223eb4db03e3d935caaba7
razers211 <mega.diable@gmail.com>
2021-08-02 16:44:57 +0100

    update

---
+++ git-push.sh
@@ -0,0 +1,21 @@
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+#read input
+input="update"
+# Committing to the local repository with a message containing the commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
